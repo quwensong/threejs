0f56d6cb8b4dc11977741049966c0e7445b2d1ca
quwensong <1607186824@qq.com>
2023-11-19 11:04:14 +0800

    ✨ feat: 增加three官方文档离线

--- three01/src/components/HelloWorld.vue
+++ three01/src/components/HelloWorld.vue
@@ -4,7 +4,7 @@
 
 <script>
 import * as THREE from "three";
-// 引入Three.js扩展库
+// 引入Three.js扩展库 控制器
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 export default {
@@ -14,30 +14,46 @@ export default {
   },
   mounted() {
     // 创建场景
-    var scene = new THREE.Scene();
+    const scene = new THREE.Scene();
     // 创建相机PerspectiveCamera（视野角度,长宽比,近截面（near）和远截面（far)） 透视相机
-    var camera = new THREE.PerspectiveCamera(
+    const camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
     );
-    camera.position.z = 5; //将摄像机稍微向外移动一些
+    camera.position.z = 3; //将摄像机稍微向外移动一些
+    camera.position.y = 3; //将摄像机稍微向外移动一些
+    camera.position.x = 3; //将摄像机稍微向外移动一些
+    camera.lookAt(0, 0, 0);
     // 创建立方体
     const geometry = new THREE.BoxGeometry(); //立方体对象，包括顶点、面
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); //MeshBasicMaterial立方体的材质
-    const cube = new THREE.Mesh(geometry, material); //网格，包括一个几何体以及作用在此几何体上的材质
+    const cube = new THREE.Mesh(geometry, material); //网格，包括一个几何体以及作用在此几何体上的材质;
     // 创建渲染器
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    // document.body.appendChild( renderer.domElement );
+
+    // 添加坐标辅助器
+    const axesHelper = new THREE.AxesHelper(5);
+    axesHelper.position.set(0,0.2,0)
+    // 添加网格地面
+    const gridHelper = new THREE.GridHelper(9, 9);
+
+    // 添加轨道控制器
+    const controls = new OrbitControls(camera, renderer.domElement);
+
     this.$refs.hello.appendChild(renderer.domElement);
     scene.add(cube); //将物体添加到(0,0,0)坐标，的网格上
+    scene.add(axesHelper);
+    scene.add(gridHelper);   
+
     // 渲染函数
     function animate() {
+      controls.update();
       requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+      // cube.rotation.x += 0.01;
+      // cube.rotation.y += 0.01;
       renderer.render(scene, camera);
     }
     animate();
