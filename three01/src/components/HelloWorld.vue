f9712975111abb8ab03254fe3f48e6d58e07031b
quwensong <1607186824@qq.com>
2023-12-16 09:03:36 +0800

    🎉 init: 增加案例

--- three01/src/components/HelloWorld.vue
+++ three01/src/components/HelloWorld.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script>
+import dat from 'dat.gui'
 import * as THREE from "three";
 // 引入Three.js扩展库 控制器
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
@@ -13,6 +14,34 @@ export default {
     msg: String,
   },
   mounted() {
+      // 创建控制对象
+      const controlData = {
+        rotationSpeed: 0.01,
+        color: "#66ccff",
+        wireframe: false, //线框
+        envMap: "无",
+      };
+
+
+      // 创建dat实例对象
+      const gui = new dat.GUI();
+
+      const config = gui.addFolder("配置");
+      // config.add(controlData,"rotationSpeed",0.01,0.1,0.01);
+      // 或者下面这种
+      config.add(controlData, "rotationSpeed").min(0.01).max(0.1).step(0.01);
+      // 颜色选择器
+      // const color = gui.addFolder("颜色");
+      config.addColor(controlData, "color") ;
+      // 下拉列表
+      config.add(controlData, "envMap",['无','全反射','漫反射']);
+      // true false
+      config.add(controlData, "wireframe" );
+
+      config.domElement.id = 'gui'
+      config.open();
+
+
     // 创建场景
     const scene = new THREE.Scene();
     // 创建相机PerspectiveCamera（视野角度,长宽比,近截面（near）和远截面（far)） 透视相机
@@ -28,7 +57,7 @@ export default {
     camera.lookAt(0, 0, 0);
     // 创建立方体
     const geometry = new THREE.BoxGeometry(); //立方体对象，包括顶点、面
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); //MeshBasicMaterial立方体的材质
+    const material = new THREE.MeshBasicMaterial({ color:controlData.color}); //MeshBasicMaterial立方体的材质
     const cube = new THREE.Mesh(geometry, material); //网格，包括一个几何体以及作用在此几何体上的材质;
     // 创建渲染器
     const renderer = new THREE.WebGLRenderer();
@@ -43,20 +72,40 @@ export default {
     // 添加轨道控制器
     const controls = new OrbitControls(camera, renderer.domElement);
 
+    this.$refs.hello.appendChild(config.domElement);
+
     this.$refs.hello.appendChild(renderer.domElement);
+
+    
     scene.add(cube); //将物体添加到(0,0,0)坐标，的网格上
     scene.add(axesHelper);
     scene.add(gridHelper);   
 
+
+
+
     // 渲染函数
     function animate() {
       controls.update();
+      //  requestAnimationFrame 会自动随着屏幕的刷新频率自动执行
       requestAnimationFrame(animate);
-      // cube.rotation.x += 0.01;
-      // cube.rotation.y += 0.01;
+      cube.rotation.x += controlData.rotationSpeed;
+      cube.rotation.y += controlData.rotationSpeed;
+      cube.material.color = new THREE.Color(controlData.color);
+      material.wireframe = controlData.wireframe
+
       renderer.render(scene, camera);
     }
     animate();
   },
 };
 </script>
+
+<style>
+
+#gui{
+  position: absolute;
+  right: 0;
+  width: 300px;
+}
+</style>
